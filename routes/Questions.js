05c92ed93da393a6204e25aa065b78d69f900bfd
MatthiasvsGitHub <matthias@schmettow.de>
2021-06-24 10:55:46 +0200

    updated header total count for users and questions/all

--- routes/Questions.js
+++ routes/Questions.js
@@ -8,71 +8,74 @@ const verifyToken = require('./verifyToken');
 
 questions.get('/questions/all', (req, res) => {
     client.query('SELECT * FROM questions')
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => {
+            res.set("X-Total-Count", data.rows.length)
+            res.json(data.rows)
+        })
+        .catch(err => res.json(err))
 })
 
 questions.get('/questions/count', (req, res) => {
     client.query('SELECT COUNT(*) FROM questions')
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 questions.get('/questions', (req, res) => {
     client.query('SELECT * FROM questions WHERE approved=1 ORDER BY RANDOM() LIMIT 10')
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 questions.post('/questions', (req, res) => {
-    const {type, category, difficulty, question, correct_answer, incorrect_answers, approved, Users_id} = req.body
+    const { type, category, difficulty, question, correct_answer, incorrect_answers, approved, Users_id } = req.body
     if (!type || !category || !difficulty || !question || !correct_answer || !incorrect_answers || !approved || !Users_id) return res.json('Not all required Fields are filled out')
     const query = 'INSERT INTO questions (type, category, difficulty, question, correct_answer, incorrect_answers, approved, Users_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *'
     const values = [type, category, difficulty, question, correct_answer, incorrect_answers, approved, Users_id]
     client.query(query, values)
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 questions.get('/questions/submitted/:user', (req, res) => {
-    const {user} = req.params
+    const { user } = req.params
     const query = 'SELECT COUNT(*) FROM questions INNER JOIN users ON users.id=Users_id WHERE questions.approved=1 AND Users_id=$1'
     const values = [user]
     client.query(query, values)
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 
 questions.get('/questions/:id', (req, res) => {
     client.query('SELECT * FROM questions WHERE id=$1', [req.params.id])
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 questions.put('/questions/:id/confirm', (req, res) => {
     const values = [req.params.id, req.body.approved]
     if (!req.body.approved) return res.json('Field approved is required')
     client.query('UPDATE questions SET approved=$2 WHERE id=$1 RETURNING *', values)
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 questions.put('/questions/:id', (req, res) => {
-    const {type, category, difficulty, question, correct_answer, incorrect_answers, approved, Users_id} = req.body
-    const {id} = req.params
+    const { type, category, difficulty, question, correct_answer, incorrect_answers, approved, Users_id } = req.body
+    const { id } = req.params
     if (!type || !category || !difficulty || !question || !correct_answer || !incorrect_answers || !approved || !Users_id) return res.json('Not all required Fields are filled out')
     const query = 'UPDATE questions SET type=$2, category=$3, difficulty=$4, question=$5, correct_answer=$6, incorrect_answers=$7, approved=$8, Users_id=$9 WHERE id=$1 RETURNING *'
     const values = [id, type, category, difficulty, question, correct_answer, incorrect_answers, approved, Users_id]
     client.query(query, values)
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 questions.delete('/questions/:id', verifyToken, (req, res) => {
     client.query('DELETE FROM questions WHERE id=$1', [req.params.id])
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 
--- routes/Users.js
+++ routes/Users.js
@@ -8,68 +8,70 @@ const verifyToken = require('./verifyToken');
 
 users.get('/users', (req, res) => {
     client.query(`SELECT id, nickname, email, points, answered, correct FROM users`)
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => {
+            res.set("X-Total-Count", data.rows.length)
+            res.json(data.rows)
+        })
+        .catch(err => res.json(err))
 })
-
 users.get('/users/count', (req, res) => {
     client.query('SELECT COUNT(*) FROM users')
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 users.get('/users/answered', (req, res) => {
     client.query('SELECT SUM(answered) FROM users')
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 users.get('/users/correct', (req, res) => {
     client.query('SELECT SUM(correct) FROM users')
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 users.post('/users/login', async (req, res) => {
-    const {email, pw} = req.body
+    const { email, pw } = req.body
     if (!pw || !email) return res.json('pw and email is required')
     const user = await client.query("SELECT * FROM users WHERE email=$1", [email])
     if (!user.rows[0]) return res.json('Email does not exists')
     const comparePassword = await bcrypt.compare(pw, user.rows[0].pw)
     if (!comparePassword) return res.status(400).send('Wrong password')
-    const token = jwt.sign({user}, process.env.SECRET, { expiresIn: '1h' })
+    const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: '1h' })
     res.header('auth-token', token)
     res.json(token)
 })
 
 users.post('/users/thisuser', verifyToken, (req, res) => {
-    res.json(req.user.user.rows[0])  
+    res.json(req.user.user.rows[0])
 })
 
 users.get('/users/:id', (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
     const query = 'SELECT id, nickname, email, points, answered, correct FROM users WHERE id=$1'
     const values = [id]
     client.query(query, values)
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 users.put('/users/:id/pw', async (req, res) => {
-    const {id} = req.params
-    const {pw} = req.body
+    const { id } = req.params
+    const { pw } = req.body
     if (!pw) return res.json('password is required')
     const query = 'UPDATE users SET pw=$2 WHERE id=$1 RETURNING id, nickname, email, points, answered, correct'
     const salt = await bcrypt.genSalt(10)
     const hashPassword = await bcrypt.hash(pw, salt)
     const values = [id, hashPassword]
     client.query(query, values)
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 users.post('/users/register', async (req, res) => {
-    const {nickname, email, pw, points} = req.body
+    const { nickname, email, pw, points } = req.body
     if (!nickname || !email || !pw || !points) return res.json('Nickname, Email, Password and Points are required')
     const salt = await bcrypt.genSalt(10)
     const hashPassword = await bcrypt.hash(pw, salt)
@@ -78,31 +80,31 @@ users.post('/users/register', async (req, res) => {
     await client.query("SELECT email FROM users WHERE email=$1", [email]).then(data => {
         if (data.rows.length > 0) return res.json('Email already exists')
         else client.query(query, values)
-        .then(data => res.json(data.rows))
-        .catch(err => res.json(err))
+            .then(data => res.json(data.rows))
+            .catch(err => res.json(err))
     })
 })
 
 users.put('/users/:id/points', (req, res) => {
-    const { points, answered, correct} = req.body
+    const { points, answered, correct } = req.body
     if (!points || !answered || !correct) return res.json('Points, answered and correct are required')
-    const {id} = req.params
+    const { id } = req.params
     const query = 'UPDATE users SET points=$2, answered=$3, correct=$4 WHERE id=$1 RETURNING points, answered, correct'
     const values = [id, points, answered, correct]
     client.query(query, values)
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 users.put('/users/:id', (req, res) => {
-    const {nickname, email, pw, points, answered, correct} = req.body
+    const { nickname, email, pw, points, answered, correct } = req.body
     if (!nickname || !email || !points || !answered || !correct) return res.json('Nickname, Email, Password, points, answered and correct are required')
-    const {id} = req.params
+    const { id } = req.params
     const query = 'UPDATE users SET nickname=$2, email=$3, points=$4, answered=$5, correct=$6 WHERE id=$1 RETURNING id, nickname, email, points, answered, correct'
     const values = [id, nickname, email, points, answered, correct]
     client.query(query, values)
-    .then(data => res.json(data.rows))
-    .catch(err => res.json(err))
+        .then(data => res.json(data.rows))
+        .catch(err => res.json(err))
 })
 
 module.exports = users
