cd3caf5a03c9c5b5007f27fd8d0b947fe3bda6e0
MatthiasvsGitHub <matthias@schmettow.de>
2021-06-28 11:08:37 +0200

    added socket.io stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require("http");
 const express = require('express')
 const app = express()
 const helmet = require("helmet");
@@ -5,9 +6,25 @@ const port = process.env.PORT || 3002
 const users = require('./routes/Users')
 const questions = require('./routes/Questions')
 const leaderboard = require('./routes/Leaderboard')
+const socketIo = require("socket.io");
+const { addUser, removeUser, getUsersInRoom } = require("./routes/Chat/users");
+const { addMessage, getMessagesInRoom } = require("./routes/Chat/message");
+
+
 const cors = require('cors')
 const path = require('path');
 app.use(helmet())
+
+//Message Server
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
+});
+//Cors
 app.use(cors())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
@@ -15,7 +32,58 @@ app.use((req, res, next) => {
     res.header('Access-Control-Expose-Headers', 'Content-Range')
     next()
 })
+//More Message and Socket.io
+const USER_JOIN_CHAT_EVENT = "USER_JOIN_CHAT_EVENT";
+const USER_LEAVE_CHAT_EVENT = "USER_LEAVE_CHAT_EVENT";
+const NEW_CHAT_MESSAGE_EVENT = "NEW_CHAT_MESSAGE_EVENT";
+const START_TYPING_MESSAGE_EVENT = "START_TYPING_MESSAGE_EVENT";
+const STOP_TYPING_MESSAGE_EVENT = "STOP_TYPING_MESSAGE_EVENT";
 
+io.on("connection", (socket) => {
+    console.log(`${socket.id} connected`);
+    // Join a conversation
+    const { roomId, name, picture } = socket.handshake.query;
+    socket.join(roomId);
+    const user = addUser(socket.id, roomId, name);
+    io.in(roomId).emit(USER_JOIN_CHAT_EVENT, user);
+    // Listen for new messages
+    socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+        const message = addMessage(roomId, data);
+        io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, message);
+    });
+    // Listen typing events
+    socket.on(START_TYPING_MESSAGE_EVENT, (data) => {
+        io.in(roomId).emit(START_TYPING_MESSAGE_EVENT, data);
+    });
+    socket.on(STOP_TYPING_MESSAGE_EVENT, (data) => {
+        io.in(roomId).emit(STOP_TYPING_MESSAGE_EVENT, data);
+    });
+    // Leave the room if the user closes the socket
+    socket.on("disconnect", () => {
+        removeUser(socket.id);
+        io.in(roomId).emit(USER_LEAVE_CHAT_EVENT, user);
+        socket.leave(roomId);
+
+        socket.on("disconnect", (reason) => {
+            if (reason === "io server disconnect") {
+                // the disconnection was initiated by the server, you need to reconnect manually
+                socket.connect();
+            }
+            // else the socket will automatically try to reconnect
+        });
+    });
+});
+//Socket.io Paths
+app.get("/rooms/:roomId/users", (req, res) => {
+    const users = getUsersInRoom(req.params.roomId);
+    return res.json({ users });
+  });
+  
+  app.get("/rooms/:roomId/messages", (req, res) => {
+    const messages = getMessagesInRoom(req.params.roomId);
+    return res.json({ messages });
+  });
+// End of Socket.io
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './documentation.html'))
@@ -24,6 +92,5 @@ app.get('/', (req, res) => {
 app.use('/', leaderboard)
 app.use('/', users)
 app.use('/', questions)
-
 
 app.listen(port, console.log(`Server is listening on port ${port}`))
