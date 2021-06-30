0d0d032c520007cb12388cfb8bd28af9f993f815
MatthiasvsGitHub <matthias@schmettow.de>
2021-06-30 11:59:36 +0200

    updated change pw route

--- routes/Users.js
+++ routes/Users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const verifyToken = require('./verifyToken');
 const express = require('express')
-users.use(express.json( {strict: false} )) 
+users.use(express.json({ strict: false }))
 users.use(express.urlencoded({ extended: true }))
 
 
@@ -61,15 +61,21 @@ users.get('/users/:id', (req, res) => {
 
 users.put('/users/:id/pw', async (req, res) => {
     const { id } = req.params
-    const { pw } = req.body
-    if (!pw) return res.json('password is required')
-    const query = 'UPDATE users SET pw=$2 WHERE id=$1 RETURNING id, nickname, email, points, answered, correct'
-    const salt = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(pw, salt)
-    const values = [id, hashPassword]
-    client.query(query, values)
-        .then(data => res.json(data.rows))
-        .catch(err => res.json(err))
+    const { pw, newPw, email } = req.body
+    if (!pw || !newPw || !email) return res.json('Old / New Password and Email is required')
+    const user = await client.query("SELECT * FROM users WHERE email=$1", [email])
+    if (!user.rows[0]) return res.json('Email does not exists')
+    const comparePassword = await bcrypt.compare(pw, user.rows[0].pw)
+    if (!comparePassword) return res.status(400).send('Wrong password')
+    else {
+        const query = 'UPDATE users SET pw=$2 WHERE id=$1 RETURNING id, nickname, email, points, answered, correct'
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(newPw, salt)
+        const values = [id, hashPassword]
+        client.query(query, values)
+            .then(data => res.json(data.rows))
+            .catch(err => res.json(err))
+    }
 })
 
 users.post('/users/register', async (req, res) => {
